6a8ec33ced84f2d740a7b8d91cdebaeb1547437c
AristidesMelendez <aristides.melendez@admios-sa.com>
2016-09-09 17:20:53 -0500

    Create dummy data.

--- Validations.scala
+++ Validations.scala
@@ -2,10 +2,7 @@ import scala.collection.mutable.ArraySeq
 import scala.collection.mutable.Buffer
 import scala.collection.mutable.ArrayBuffer
 
-class Cell(k: String, v: Any) {
-  def key: String = k
-  def value: Any = v
-}
+case class Cell(key: String, value: Any)
 
 class Row(c: Buffer[Cell]) {
   def cells = c
@@ -17,53 +14,31 @@ object Validations {
     // Run Functions
     println("Validations!")
 
-  var cell = new Cell("Color", "White")
-  println("The row has " + cell.key + " of " + cell.value)
-  val row = createRow//()
+  var table = createTable();
+  println("new Table: " + table)
   }
 
-  def createRow : Row = {
-    // Create an array of 10 objects
+  def createRow(color: String, size: String, price: Number, isNew: Boolean, serial: String, model: String) : Row = {
+    // Create a row of cells
     var row = new Row(new ArrayBuffer[Cell](10))
-    row.cells += new Cell("Color", "Black")
-    println("Number of cells " + row.cells.length)
-    println("New cell " + row.cells(0).value)
-    row.cells += new Cell("Color", "Blue")
-    println("Number of cells " + row.cells.length)
-    println("New cell " + row.cells(1).value)
-    row.cells += new Cell("Color", "Red")
-    println("Number of cells " + row.cells.length)
-    println("New cell " + row.cells(2).value)
-
+    row.cells += new Cell("color", color)
+    row.cells += new Cell("size", size)
+    row.cells += new Cell("price", price)
+    row.cells += new Cell("isNew", isNew)
+    row.cells += new Cell("serial", serial)
+    row.cells += new Cell("model", model)
     row
   }
 
-  def id(x: Int, message: String)  = {
-    println(message)
-    x
+  def createTable() : Buffer[Row] = {
+    var table = new ArrayBuffer[Row](5)
+
+    table += createRow("Black", "5", 40.00, true, "asdaskr", "Elantra")
+    table += createRow("Blue", "1", 50.00, true, "1234dfqwer", "Yaris")
+    table += createRow("Gree", "7", 30.00, false, "ariupip", "Chevy")
+    table += createRow("Red", "3", 20.00, true, "mjkojpia", "Sentra")
+    table += createRow("Orange", "8", 100.00, false, "agweqwer", "fairly lady Z")
+    table
   }
 
-  def square(x: Int, message: String): Int = {
-    println(message + "square")
-    x * x
-  }
-
-  def powerOfTwo(x: Int, message: String) : Int = {
-    println(message + "Power of Two: " + x)
-    if (x == 0) 1 else 2 * powerOfTwo(x - 1, message + (x - 1))
-  }
-
-  def sum(f: (Int, String) => Int, a: Int, b: Int): Int = {
-    if (a > b) 0 else f(a, "Sum ") + sum(f, a + 1, b)
-  }
-
-  def suma(f: Int => Int): (Int, Int) => Int = {
-    def sumFunc(a: Int, b: Int): Int =
-      if (a > b) 0 else f(a) + sumFunc(a + 1, b)
-  sumFunc
-  }
-
-  def sumInts(a: Int, b: Int): Int = sum(id, a, b)
-  def sumSquares(a: Int, b: Int) : Int = sum(square, a, b)
-  def sumPowersOfTwo(a: Int, b: Int): Int = sum(powerOfTwo, a, b)
 }
